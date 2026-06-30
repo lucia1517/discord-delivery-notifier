34e542512f74e066c9eb960a2116f2eee75d018a
lucia1517 <royroy0315@gmail.com>
2026-06-30 22:23:56 +0900

    feat: implement Express server

--- src/config.js
+++ src/config.js
@@ -23,6 +23,34 @@ export const CONFIG = {
 
         port: Number(process.env.PORT) || 8080
 
+    },
+
+    gmail: {
+
+        userId: "me",
+
+        topicName: process.env.GMAIL_TOPIC_NAME ?? "",
+
+        historyTypes: [
+
+            "messageAdded"
+
+        ]
+
+    },
+
+    discord: {
+
+        webhookUrl: process.env.DISCORD_WEBHOOK_URL ?? ""
+
+    },
+
+    line: {
+
+        channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN ?? "",
+
+        userId: process.env.LINE_USER_ID ?? ""
+
     }
 
 };
